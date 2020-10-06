b75d4a9962c8bb8aacf68177c81fd9f30b297e26
Zhenyu Song <sunnyszy@gmail.com>
2020-10-06 13:03:57 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -32,10 +32,10 @@ pc.defineParameter("osImage", "Select OS image",
                    "default")
 
 # Optional physical type for all nodes.
-#pc.defineParameter("phystype",  "Optional physical node type",
-#                   portal.ParameterType.STRING, "c220g1",
-#                   longDescription="Specify a physical node type (pc3000,d710,etc) " +
-#                   "instead of letting the resource mapper choose for you.")
+pc.defineParameter("phystype",  "Optional physical node type",
+                  portal.ParameterType.STRING, "c220g1",
+                  longDescription="Specify a physical node type (pc3000,d710,etc) " +
+                  "instead of letting the resource mapper choose for you.")
 
 # Optionally create XEN VMs instead of allocating bare metal nodes.
 pc.defineParameter("useVMs",  "Use XEN VMs",
